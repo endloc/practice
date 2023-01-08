6e2bbc4d2f7047b821e555ce7530e185667cc724
endloc <li.amirova.l@gmail.com>
2023-01-08 16:28:34 +0300

    (#5)new program: keyboard input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int numbers[] = new int[]{2, 5, 8, -8, 19, 2004, -10000};
-        int size = numbers.length;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+        int size = input.nextInt();
+        int numbers[] = new int[size];
+        System.out.println("Insert array elements:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = input.nextInt();
+        }
         java.util.Arrays.sort(numbers);
         System.out.println("Max element is " + numbers[size - 1]);
     }
